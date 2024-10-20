063a4668de157291c49d1539b762861575ecbba4
Dmytry64 <nosdm64@gmail.com>
2024-10-20 08:59:24 +0300

    feat: добавляет стили

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,26 +1,32 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes/routes";
-import { Layout } from "../pages/layout";
-import { Catalog } from "src/pages/catalog";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes/routes';
+import { Layout } from '../pages/layout';
+import { Catalog } from 'src/pages/catalog';
+import QuestPage from 'src/pages/quest-page/ui/quest-page';
 
 
-const App = () : JSX.Element => {
+const App = (): JSX.Element => {
 
-    const router = createBrowserRouter([
+  const router = createBrowserRouter([
+    {
+      path: AppRoutes.Catalog,
+      element: <Layout />,
+      children: [
         {
-            path: AppRoutes.Catalog,
-            element: <Layout/>,
-            children: [
-              {
-                path: AppRoutes.Catalog,
-                element: <Catalog />
-              }
-            ]
+          path: AppRoutes.Catalog,
+          element: <Catalog />
+        },
+
+        {
+          path: `${AppRoutes.Quest}/:questId`,
+          element: <QuestPage/>
         }
-    ])
-    return (
-        <RouterProvider router = {router}/>
-    )
-}
+      ]
+    }
+  ]);
+  return (
+    <RouterProvider router={router} />
+  );
+};
 
 export default App;
